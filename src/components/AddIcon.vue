f822dc725500187ab0c7e06a6e2931c4dfd13c88
zhangjingyi <15626168865@163.com>
2019-01-13 18:39:15 +0800

    fix: 修复报错bug

--- src/components/AddIcon.vue
+++ src/components/AddIcon.vue
@@ -1,46 +1,45 @@
 <template>
-  <div class="add-wrap">
-    <span
-      class="iconfont icon-plus-circle"
-      @click="addHandle"
-    ></span>
-  </div>
+	<div class="add-wrap">
+		<span class="iconfont icon-plus-circle" @click="addHandle"></span>
+	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    from: String
-  },
-  methods: {
-    addHandle () {
-      switch (this.from) {
-        case "TaskITEM":
-          this.$emit('addHandle')
-          break
-      
-        case "Index":
-          this.$root.Bus.emit('addTaskItem')
-          break
-      }
-    }
-  }
+	props: {
+		from: String
+	},
+	methods: {
+		addHandle() {
+			switch (this.from) {
+				case "TaskITEM":
+					this.$root.Bus.$emit("addTaskItem");
+
+					break;
+
+				case "Index":
+					this.$emit("addHandle");
+
+					break;
+			}
+		}
+	}
 };
 </script>
 
 <style scoped>
 .add-wrap {
-  text-align: center;
+	text-align: center;
 }
 .icon-plus-circle {
-  padding: 5px;
+	padding: 5px;
 }
 </style>
 
 <style>
 .add-wrap .icon-plus-circle {
-  font-size: 34px;
-  color: #bdd7d8;
+	font-size: 34px;
+	color: #bdd7d8;
 }
 </style>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,101 +1,101 @@
 <template>
-  <div class="app-wrap">
-    <TaskItem
-      v-for='(item, i) in list'
-      :id='i'
-      :title='item.title'
-      :progress='item.progress'
-      @delHandle='delHandle(i)'
-      @progressHandle='progressHandle'
-      @titleHandle='titleHandle'
-      @upHandle='upHandle'
-      @downHandle='downHandle'
-    />
-    <AddIcon :from="Index" @addHandle="addHandle"/>
-  </div>
+	<div class="app-wrap">
+		<TaskItem
+			v-for="(item, i) in list"
+			:id="i"
+			:title="item.title"
+			:progress="item.progress"
+			@delHandle="delHandle(i)"
+			@progressHandle="progressHandle"
+			@titleHandle="titleHandle"
+			@upHandle="upHandle"
+			@downHandle="downHandle"
+		/>
+		<AddIcon from="Index" @addHandle="addHandle"/>
+	</div>
 </template>
 
 <script>
-import TaskItem from "../components/TaskItem"
-import AddIcon from '../components/addIcon'
+import TaskItem from "../components/TaskItem";
+import AddIcon from "../components/addIcon";
 
 export default {
-  components: {
-    TaskItem,
-    AddIcon
-  },
-  data() {
-    return {
-      list: []
-    };
-  },
-  updated() {
-    if (this.list.length === 0) {
-      return;
-    }
-    localStorage.setItem(
-      "list",
-      JSON.stringify({
-        data: this.list
-      })
-    );
-  },
-  mounted() {
-    const list = localStorage.getItem("list");
-    this.list = (list && JSON.parse(list).data) || [];
-  },
-  methods: {
-    addHandle() {
-      this.list.push({
-        title: "",
-        progress: 0
-      });
-    },
-    delHandle(id) {
-      for (let l = this.list.length; l--; ) {
-        const item = this.list[l];
-        if (l === id) {
-          this.list.splice(l, 1);
-          break;
-        }
-      }
-    },
-    progressHandle(value, id) {
-      for (let l = this.list.length; l--; ) {
-        const item = this.list[l];
-        if (l === id) {
-          item.progress = value;
-          break;
-        }
-      }
-    },
-    titleHandle(value, id) {
-      for (let l = this.list.length; l--; ) {
-        const item = this.list[l];
-        if (l === id) {
-          item.title = value;
-          break;
-        }
-      }
-    },
-    upHandle(id) {
-      if (id === 0) {
-        return;
-      }
-      const temp = this.list[id - 1];
-      this.$set(this.list, id - 1, this.list[id]);
-      this.$set(this.list, id, temp);
-    },
-    downHandle(id) {
-      console.log(id);
-      if (id === this.list.length - 1) {
-        return;
-      }
-      const temp = this.list[id + 1];
-      this.$set(this.list, id + 1, this.list[id]);
-      this.$set(this.list, id, temp);
-    }
-  }
+	components: {
+		TaskItem,
+		AddIcon
+	},
+	data() {
+		return {
+			list: []
+		};
+	},
+	updated() {
+		if (this.list.length === 0) {
+			return;
+		}
+		localStorage.setItem(
+			"list",
+			JSON.stringify({
+				data: this.list
+			})
+		);
+	},
+	mounted() {
+		const list = localStorage.getItem("list");
+		this.list = (list && JSON.parse(list).data) || [];
+	},
+	methods: {
+		addHandle() {
+			this.list.push({
+				title: "",
+				progress: 0
+			});
+		},
+		delHandle(id) {
+			for (let l = this.list.length; l--; ) {
+				const item = this.list[l];
+				if (l === id) {
+					this.list.splice(l, 1);
+					break;
+				}
+			}
+		},
+		progressHandle(value, id) {
+			for (let l = this.list.length; l--; ) {
+				const item = this.list[l];
+				if (l === id) {
+					item.progress = value;
+					break;
+				}
+			}
+		},
+		titleHandle(value, id) {
+			for (let l = this.list.length; l--; ) {
+				const item = this.list[l];
+				if (l === id) {
+					item.title = value;
+					break;
+				}
+			}
+		},
+		upHandle(id) {
+			if (id === 0) {
+				return;
+			}
+			const temp = this.list[id - 1];
+			this.$set(this.list, id - 1, this.list[id]);
+			this.$set(this.list, id, temp);
+		},
+		downHandle(id) {
+			console.log(id);
+			if (id === this.list.length - 1) {
+				return;
+			}
+			const temp = this.list[id + 1];
+			this.$set(this.list, id + 1, this.list[id]);
+			this.$set(this.list, id, temp);
+		}
+	}
 };
 </script>
 
@@ -103,7 +103,7 @@ export default {
 
 <style scoped>
 .app-wrap {
-  height: 100%;
-  padding-top: 10px;
+	height: 100%;
+	padding-top: 10px;
 }
 </style>
